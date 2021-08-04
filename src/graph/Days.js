15288fdd3272eeb12b3952f8cf45a985395c830d
Paulo Fortes <pauloffn@gmail.com>
2021-08-04 13:34:55 -0300

    ...

--- src/graph/Days.js
+++ src/graph/Days.js
@@ -23,7 +23,7 @@ export default function Days({
                 data-count={d.count}
                 data-date={formatDate(d.date)}
                 onClick={() => onClick(d)}
-                onMouseOver={() => onHover(d)}
+                onMouseOver={() => onHover(d)}                
               />
             ))}
           </g>
